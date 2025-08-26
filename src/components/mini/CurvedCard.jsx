2fc85ffbcb0e5bc6352899e87be771e64e15f7f4
VincentNdegwa <ndegwavincent7@gmail.com>
2025-08-26 20:12:37 +0300

    feat: remove stroke color from CurvedCard component for improved styling consistency

--- src/components/mini/CurvedCard.jsx
+++ src/components/mini/CurvedCard.jsx
@@ -14,7 +14,6 @@ const CurvedCard = ({ children, color = "#fff", invert = false }) => (
       <path
         d="M60 0 H1140 Q1200 0 1200 60 V740 Q1200 800 1140 800 H600 Q580 800 560 780 L540 760 Q520 740 500 740 H60 Q0 740 0 680 V60 Q0 0 60 0 Z"
         fill={color}
-        stroke="#101010"
         strokeWidth="2"
       />
     </svg>
